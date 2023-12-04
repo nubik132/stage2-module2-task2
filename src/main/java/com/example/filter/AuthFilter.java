0091a8943fa1764cad708d87b1cf7d69aad2368b
nubik132 <mister.nubik@list.ru>
2023-12-04 17:42:15 +0300

    feat: complete task

--- src/main/java/com/example/filter/AuthFilter.java
+++ src/main/java/com/example/filter/AuthFilter.java
@@ -1,5 +1,17 @@
 package com.example.filter;
 
-public class AuthFilter {
-    //write your code here!
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/user/*"})
+public class AuthFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if (request.getAttribute("user") == null) {
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/login.jsp");
+            dispatcher.forward(request, response);
+        }
+    }
 }
--- src/main/java/com/example/servlet/LoginServlet.java
+++ src/main/java/com/example/servlet/LoginServlet.java
@@ -1,5 +1,51 @@
 package com.example.servlet;
 
-public class LoginServlet {
+import com.example.Users;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (!isSessionAttributeExist(req)) {
+            RequestDispatcher dispatcher = req.getRequestDispatcher("/login.jsp");
+            dispatcher.forward(req, resp);
+        } else {
+            RequestDispatcher dispatcher = req.getRequestDispatcher("/user/hello.jsp");
+            dispatcher.forward(req, resp);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+        List<String> users = Users.getInstance().getUsers();
+
+        if (users.contains(login) &&
+                !password.isEmpty()) {
+            req.setAttribute("user", login);
+            RequestDispatcher dispatcher = req.getRequestDispatcher("/user/hello.jsp");
+            dispatcher.forward(req, resp);
+        }
+        else {
+            RequestDispatcher dispatcher = req.getRequestDispatcher("/login.jsp");
+            dispatcher.forward(req, resp);
+        }
+    }
+
+    private boolean isSessionAttributeExist(HttpServletRequest req) {
+        HttpSession session = req.getSession();
+        return session.getAttribute("user") != null;
+    }
     //write your code here!
 }
